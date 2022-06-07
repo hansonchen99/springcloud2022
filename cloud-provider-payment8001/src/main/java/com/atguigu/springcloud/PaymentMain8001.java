295dcfe0dc9aad3f0e81d173e945ba69637df3ca
chenlei <1665614539@qq.com>
2022-06-07 14:40:43 +0800

    支付微服务模块开发

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
@@ -0,0 +1,17 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chenlei
+ * @date 2022-06-07 14:35
+ * @description
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
+}
